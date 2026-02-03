f11b194d88ce398c9b51867b14d99763ae51a368
modernscape <modernscape@gmail.com>
2026-02-03 13:41:25 +0900

    globals.cssコメント失礼いたします。

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 // projects/sphere/index.tsx
 import Scene from "./components/Scene" // 相対パスに修正
-import "../projects/sphere/app/globals.css" // 必要なら読み込み（後述）
+// import "../projects/sphere/app/globals.css" // 必要なら読み込み（後述）
 
 export default function SphereProject() {
   return (
